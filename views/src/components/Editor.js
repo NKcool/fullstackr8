df20f5f82089b1f6af7869ac3fe8bbb22f7f4aa7
master-dhanesh <dhanesh1296@gmail.com>
2023-02-15 17:31:26 +0530

    Editor working , image upload error

--- views/src/components/Editor.js
+++ views/src/components/Editor.js
@@ -1,76 +1,34 @@
-import React, { useState } from "react";
 import { createReactEditorJS } from "react-editor-js";
-import Header from "@editorjs/header";
-import CodeTool from "@editorjs/code";
-import List from "@editorjs/list";
-import Embed from "@editorjs/embed";
-import ImageTool from "@editorjs/image";
-import Quote from "@editorjs/quote";
-import Link from "@editorjs/link";
 
-export const EDITOR_JS_TOOLS = {
-    header: {
-        header: {
-            class: Header,
-            inlineToolbar: ["link"],
-            config: {
-                placeholder: "Enter a header",
-                levels: [1, 2, 3, 4, 5, 6],
-                defaultLevel: 1,
-            },
-        },
-        // list: {
-        //     class: List,
-        //     inlineToolbar: ["link", "bold"],
-        //     config: {
-        //         defaultStyle: "unordered",
-        //     },
-        // },
-        //     embed: {
-        //         class: Embed,
-        //         inlineToolbar: false,
-        //         config: {
-        //             services: {
-        //                 youtube: true,
-        //             },
-        //         },
-        //     },
-        //     code: CodeTool,
-        //     image: {
-        //         class: ImageTool,
-        //         config: {
-        //             endpoints: {
-        //                 byFile: "http://localhost:3000/uploadFile",
-        //                 byUrl: "http://localhost:3000/fetchUrl",
-        //             },
-        //             field: "blog",
-        //             type: "image/*",
-        //         },
-        //     },
-        //     quote: {
-        //         class: Quote,
-        //         inlineToolbar: true,
-        //         config: {
-        //             quotePlaceholder: "Enter a quote",
-        //             captionPlaceholder: "Quote's author",
-        //         },
-        //     },
-    },
-};
+import { useRef, useCallback } from "react";
+import { EDITOR_JS_TOOLS } from "./constants";
 
-const Editor = () => {
-    const [editor, setEditor] = useState(null);
-    const ReactEditorJS = createReactEditorJS();
+const ReactEditorJS = createReactEditorJS();
+
+function App() {
+    const editorJS = useRef(null);
+    const handleInitialize = useCallback((instance) => {
+        editorJS.current = instance;
+    }, []);
+
+    const handleSave = useCallback(async () => {
+        const savedData = await editorJS.current.save();
+        console.log(savedData);
+    }, []);
+
+    const handleClear = useCallback(async () => {
+        await editorJS.current.clear();
+    }, []);
     return (
-        <ReactEditorJS
-            holder="custom"
-            value={editor}
-            onChange={(e) => setEditor(e)}
-            tools={EDITOR_JS_TOOLS}
-        >
-            <div id="custom"></div>
-        </ReactEditorJS>
+        <div>
+            <ReactEditorJS
+                tools={EDITOR_JS_TOOLS}
+                onInitialize={handleInitialize}
+            />
+            <button onClick={handleSave}>Save</button>
+            <button onClick={handleClear}>Clear</button>
+        </div>
     );
-};
+}
 
-export default Editor;
+export default App;
